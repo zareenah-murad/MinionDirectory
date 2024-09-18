51ad7ab906da75e4a4f0062a88cc3a69f9cdf027
zareenah <zmurad@smu.edu>
2024-09-18 11:38:50 -0500

    Initial Commit

---
+++ MinionDirectory/MinionDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinionDirectoryApp.swift
+//  MinionDirectory
+//
+//  Created by Zareenah Murad on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MinionDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
