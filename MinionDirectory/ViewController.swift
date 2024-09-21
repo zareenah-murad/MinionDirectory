55d7a734aa92a991166dad9bc3f8b9ea7ec32779
zareenah <zmurad@smu.edu>
2024-09-21 13:00:14 -0500

    fixed formatting on Minion Directory, added 3 types of tableview cells

--- MinionDirectory/ViewController.swift
+++ MinionDirectory/ViewController.swift
@@ -2,38 +2,7 @@
 //  ViewController.swift
 //  MinionDirectory
 //
-//  Created by Zareenah Murad on 9/18/24.
+//  Created by Zareenah Murad on 9/20/24.
 //
 
-import UIKit
-
-class ViewController: UIViewController, UIScrollViewDelegate {
-
-    lazy var imageModel = {
-        return ImageModel.sharedInstance()
-    }()
-    
-    lazy private var imageView: UIImageView? = {
-        return UIImageView.init(image: self.imageModel.getImageWithName(displayImageName))
-    }()
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    @IBAction func viewDirectoryButton(_ sender: UIButton) {
-    }
-    
-    @IBAction func viewBananaStocksButton(_ sender: UIButton) {
-        // segue is handled by the storyboard connection
-    }
-
-    
-    var displayImageName = "Bob"
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-
-    
-}
-
-}
+import Foundation
